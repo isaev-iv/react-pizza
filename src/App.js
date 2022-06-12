a779ca0c023dbd6156cd9a97101a93e87ed161bf
Liam <backgroundpositioncenter@gmail.com>
2022-06-13 00:09:43 +0300

    БД перенесена в бэкенд

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React from "react";
 import "./scss/app.scss";
-import pizzas from "./assets/pizzas.json";
+// import pizzas from "./assets/pizzas.json";
 import Header from "./components/Header.jsx";
 import Categories from "./components/Categories.jsx";
 import Sort from "./components/Sort.jsx";
 import PizzaBlock from "./components/PizzaBlock";
 
 function App() {
+  const [items, setItems] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("https://62a62cccb9b74f766a44cc83.mockapi.io/items")
+      .then((res) => {
+        return res.json();
+      })
+      .then((arr) => {
+        setItems(arr);
+      });
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <div className="container">
           <div className="content__top">
-            {/* <Categories /> */}
+            <Categories />
             <Sort />
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {pizzas.map((obj) => (
+            {items.map((obj) => (
               <PizzaBlock
                 key={obj.id}
                 title={obj.title}
